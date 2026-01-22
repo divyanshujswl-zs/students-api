c26bc98090a81f9464c8122f301f1d61c0f4df14
Divyanshu Jaiswal <divyanshu.jaiswal@zopsmart.com>
2026-01-22 15:22:51 +0530

    add request to validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/divyanshujswl-zs/students-api/internal/config"
+	"github.com/divyanshujswl-zs/students-api/internal/handlers/student"
 )
 
 func main() {
@@ -23,6 +24,8 @@ func main() {
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to homepage!"))
 	})
+
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
